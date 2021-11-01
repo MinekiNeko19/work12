f5a42570b5d687d0e59e41bf2345da1bdda39cb1
Sarah Leong <sleong20@stuy.edu>
2021-11-01 12:56:48 -0400

    prints out the files in the dir

--- printdir.c
+++ printdir.c
@@ -1,3 +1,22 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    DIR *d;
+    struct dirent *dp;
+
+    d = opendir("./");
+    dp = readdir(d);
+
+    // printf("Total Directory Size: %d bytes\n", );
+    printf("files in this folder:\n");
+    while(dp) {
+        printf("\t%s\n",dp->d_name);
+        dp = readdir(d);
+    }
+    closedir(d);
+}
